cfa88554ec30d2ef4cb27f243f6542fa9e297171
vasanthpandiyan <vasanthpandiyan4059648@gmail.com>
2023-09-17 16:01:53 +0530

    ok

---
+++ react title intro/title/src/App.js
@@ -0,0 +1,13 @@
+
+import LoginComponent from "./login/login";
+
+function App() {
+  return (
+ <div style={{margin:'0px',padding:'0'}}>
+  
+  <LoginComponent />
+ </div>
+  );
+}
+
+export default App;
---
+++ react title intro/title/src/login/login.js
@@ -0,0 +1,27 @@
+ import './login.css'
+ function LoginComponent(){
+
+return(
+    <div className='img-bg'>
+        <h1> Welcome to the Login Page</h1>
+      <div className='styling'>
+        <div>Username : <input/></div>
+        <div>Password : <input/></div>
+        <button>Login</button>
+        </div>
+        
+        {/* <img src='https://source.unsplash.com/1600x300/?universe' alt='img' ></img> */}
+
+
+
+
+    </div>
+)
+}
+
+export default LoginComponent;
+
+
+
+
+
